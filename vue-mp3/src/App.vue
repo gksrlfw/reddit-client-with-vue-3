a40780d494587fd4f5076c333413a7bca5725fac
gksrlfw <gksrlfw@naver.com>
2021-02-13 16:40:26 +0900

    create project!

---
+++ vue-mp3/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="nav">hello</div>
+  <router-view />
+</template>
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const count = ref("");
+    const a = ref("");
+    return {
+      count,
+      a
+    };
+  }
+};
+</script>
---
+++ vue-mp3/src/views/Main.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div>{{ str }}</div>
+</template>
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const str = ref("main");
+    return {
+      str
+    };
+  }
+};
+</script>
+<style lang=""></style>
